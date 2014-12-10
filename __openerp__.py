45d76ca876c89d10817977a6ffc2203df10a6d44
root <root@factory.openjaf.com>
2014-12-10 21:47:28 +0200

    initial commit

---
+++ __openerp__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Website images',
+    'category': 'Website',
+    'summary': '',
+    'website': '',
+    'version': '1.0',
+    'description': """
+OpenERP Website Images
+======================
+
+        """,
+    'author': 'OpenJAF',
+    'depends': ['website'],
+    'installable': True,
+    'data': [
+        'data/data.xml',
+    'views/website_images.xml',
+    ],
+    'application': True,
+}
